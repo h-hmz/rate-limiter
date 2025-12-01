a77072629fd0904c5153fb0424879af32acc4e0a
h-hmz <88973609+h-hmz@users.noreply.github.com>
2025-12-01 18:15:44 +0100

    Add TTL tests (Redis)

--- limiter/fixedwindow/fixed_window_test.go
+++ limiter/fixedwindow/fixed_window_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -41,29 +42,29 @@ func runFixedWindowTestSuite(t *testing.T, storeFactory func() Store) {
 		limiterInstance := New(limit, windowDuration, storeFactory(), clock)
 
 		ctx := context.Background()
-		user := "Recoome"
+		key := "recoome"
 
 		// 2. Consume all tokens
 		for range limit {
-			allowed, err := limiterInstance.Allow(ctx, user)
+			allowed, err := limiterInstance.Allow(ctx, key)
 			require.NoError(t, err)
 			assert.True(t, allowed, "Request within limit should be allowed")
 		}
 
 		// 3. Exceed Limit
-		allowed, err := limiterInstance.Allow(ctx, user)
+		allowed, err := limiterInstance.Allow(ctx, key)
 		require.NoError(t, err)
 		assert.False(t, allowed, "Request exceeding limit should be rejected")
 
 		// 4. Advance Clock (Halfway) -> Should still reject
 		clock.Advance(30 * time.Second)
-		allowed, err = limiterInstance.Allow(ctx, user)
+		allowed, err = limiterInstance.Allow(ctx, key)
 		require.NoError(t, err)
 		assert.False(t, allowed, "Request in same window should be rejected")
 
 		// 5. Advance Clock (New Window) -> Should allow
 		clock.Advance(31 * time.Second) // Total > 60s
-		allowed, err = limiterInstance.Allow(ctx, user)
+		allowed, err = limiterInstance.Allow(ctx, key)
 		require.NoError(t, err)
 		assert.True(t, allowed, "Request in new window should be allowed")
 	})
@@ -75,7 +76,7 @@ func runFixedWindowTestSuite(t *testing.T, storeFactory func() Store) {
 		limiterInstance := New(limit, time.Minute, storeFactory(), clock)
 
 		ctx := context.Background()
-		user := "Jeice"
+		key := "jeice"
 
 		var successCount atomic.Int64
 		var wg sync.WaitGroup
@@ -86,7 +87,7 @@ func runFixedWindowTestSuite(t *testing.T, storeFactory func() Store) {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				isAllowed, err := limiterInstance.Allow(ctx, user)
+				isAllowed, err := limiterInstance.Allow(ctx, key)
 				require.NoError(t, err)
 				if isAllowed {
 					successCount.Add(1)
@@ -97,5 +98,43 @@ func runFixedWindowTestSuite(t *testing.T, storeFactory func() Store) {
 
 		assert.Equal(t, limit, successCount.Load(), "Should strictly enforce limit under concurrency")
 	})
+
+}
+
+func TestFixedWindowTTL_Redis(t *testing.T) {
+
+	// Setup
+	mr := miniredis.RunT(t)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+	store := NewRedisStore(mr.Addr())
+
+	start := time.Unix(int64(time.Minute), 0) // Jan 1, 1970, 00:01:00 UTC
+	clock := limiter.NewMockClock(start)
+	l := New(1, time.Minute, store, clock) // ttl = WindowDuration = 60s
+
+	ctx := context.Background()
+	key := "android17"
+
+	// Trigger key creation
+	_, err := l.Allow(ctx, key)
+	require.NoError(t, err)
+
+	exists, _ := redisClient.Exists(ctx, key).Result()
+	assert.Equal(t, int64(1), exists, "Key should exist immediately")
+
+	ttl, _ := redisClient.TTL(ctx, key).Result()
+	assert.True(t, ttl > 0, "TTL should be set")
+
+	mr.FastForward(59 * time.Second) //Fastforward time in Redis
+
+	exists, _ = redisClient.Exists(ctx, key).Result()
+	assert.Equal(t, int64(1), exists, "Key should exist after 59s")
+
+	mr.FastForward(2 * time.Second)
+
+	exists, _ = redisClient.Exists(ctx, key).Result()
+	assert.Equal(t, int64(0), exists, "Key should be evicted after 61s")
 
 }
--- limiter/tokenbucket/token_bucket_test.go
+++ limiter/tokenbucket/token_bucket_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -42,29 +43,29 @@ func runTokenBucketTestSuite(t *testing.T, storeFactory func() Store) {
 		limiterInstance := New(rate, burst, storeFactory(), clock)
 
 		ctx := context.Background()
-		user := "Raditz"
+		key := "raditz"
 
 		// 2. Consume Initial Burst (10 tokens)
 		for range burst {
-			isAllowed, err := limiterInstance.Allow(ctx, user)
+			isAllowed, err := limiterInstance.Allow(ctx, key)
 			require.NoError(t, err)
 			assert.True(t, isAllowed, "Request within burst should be allowed")
 		}
 
 		// 3. Verify Limit Reached
-		isAllowed, _ := limiterInstance.Allow(ctx, user)
+		isAllowed, _ := limiterInstance.Allow(ctx, key)
 		assert.False(t, isAllowed, "Request exceeing limit should be rejected (bucket empty)")
 
 		// 4. Advance Clock & Verify Refill
 		// Advance 1 second -> Should refill 1 token (Rate is 1/sec)
 		clock.Advance(time.Second)
 
-		isAllowed, _ = limiterInstance.Allow(ctx, user)
+		isAllowed, _ = limiterInstance.Allow(ctx, key)
 		assert.True(t, isAllowed, "Request after 1s refill should be allowed")
 
 		// 5. Verify Token Consumption
 		// That single refilled token is now gone, next request must fail
-		isAllowed, _ = limiterInstance.Allow(ctx, user)
+		isAllowed, _ = limiterInstance.Allow(ctx, key)
 		assert.False(t, isAllowed, "Request should be rejected again (refilled token consumed)")
 	})
 
@@ -76,7 +77,7 @@ func runTokenBucketTestSuite(t *testing.T, storeFactory func() Store) {
 		limiterInstance := New(0, 1, storeFactory(), wallClock)
 
 		ctx := context.Background()
-		user := "Nappa"
+		key := "nappa"
 
 		var successCount atomic.Int64
 		var wg sync.WaitGroup
@@ -87,7 +88,7 @@ func runTokenBucketTestSuite(t *testing.T, storeFactory func() Store) {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				isAllowed, _ := limiterInstance.Allow(ctx, user)
+				isAllowed, _ := limiterInstance.Allow(ctx, key)
 				if isAllowed {
 					successCount.Add(1)
 				}
@@ -97,4 +98,42 @@ func runTokenBucketTestSuite(t *testing.T, storeFactory func() Store) {
 
 		assert.Equal(t, int64(1), successCount.Load(), "Should strictly enforce limit of 1 under high concurrency")
 	})
+}
+
+func TestTokenBucketTTL_Redis(t *testing.T) {
+
+	// Setup
+	mr := miniredis.RunT(t)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+	store := NewRedisStore(mr.Addr())
+
+	start := time.Now()
+	clock := limiter.NewMockClock(start)
+	l := New(1.0, 60, store, clock) // ttl = burst/rate = 60s
+
+	ctx := context.Background()
+	key := "android18"
+
+	// Trigger key creation
+	_, err := l.Allow(ctx, key)
+	require.NoError(t, err)
+
+	exists, _ := redisClient.Exists(ctx, key).Result()
+	assert.Equal(t, int64(1), exists, "Key should exist immediately")
+
+	ttl, _ := redisClient.TTL(ctx, key).Result()
+	assert.True(t, ttl > 0, "TTL should be set")
+
+	mr.FastForward(59 * time.Second) //Fastforward time in Redis
+
+	exists, _ = redisClient.Exists(ctx, key).Result()
+	assert.Equal(t, int64(1), exists, "Key should exist after 59s")
+
+	mr.FastForward(2 * time.Second)
+
+	exists, _ = redisClient.Exists(ctx, key).Result()
+	assert.Equal(t, int64(0), exists, "Key should be evicted after 61s")
+
 }
